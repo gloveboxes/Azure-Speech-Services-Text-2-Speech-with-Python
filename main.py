88dd497f364dc4f13b7c6c11e960b5621002f875
Dave Glover (DEVELOPER RELATIONS) <dglover@microsoft.com>
2019-07-15 17:21:55 +1000

    text translation, added audio caching

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from text2speech import TextToSpeech
 import os
+from pathlib import Path
 
-key = os.environ['SPEECHKEY']
+speechKey = os.environ['SPEECHKEY']
+translatorKey = os.environ['TRANSLATOR_TEXT_KEY']
+
 
 # https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/language-support#text-to-speech
 
-t2s = TextToSpeech(key, 'en-US-JessaNeural')
+t2s = TextToSpeech(speechKey, translatorKey, 'ko', 'ko-KR-HeamiRUS')
 
 # t2s = TextToSpeech(key)
 
-t2s.play(" Hello, and welcome to Bits of Build. ")
-t2s.play(" I scanned a delicious apple.")
+# t2s.play(" Hello, and welcome to Bits of Build. ")
+t2s.play("Starting scanner.")
+t2s.play("Starting scanner.")
+t2s.play("You scanned a red apple.")
+t2s.play("You scanned an orange.")
+t2s.play("You scanned a banana.")
--- text2speech.py
+++ text2speech.py
@@ -1,61 +1,51 @@
 from azure_speech import AzureSpeechServices
-# import pyaudio
-# import wave
-# import sys
-# import io
+from azure_text_translate import TranslateText
 from pygame import mixer
 import time
-
+import hashlib
+from pathlib import Path
+import os
 
 
 class TextToSpeech():
     # Short name for 'Microsoft Server Speech Text to Speech Voice (en-US, GuyNeural)'
-    def __init__(self, azureSpeechServiceKey, voice='en-US-GuyNeural'):
+    def __init__(self, azureSpeechServiceKey, azureTranslatorServiceKey, translateTolanguage, voice='ko-KR-HeamiRUS',):
         self.translator = AzureSpeechServices(azureSpeechServiceKey, voice)
+        self.translateText = TranslateText(
+            azureTranslatorServiceKey, translateTolanguage)
         self.ttsAudio = {}
         mixer.init(frequency=16000, size=-16, channels=1)
-
-    # def _playAudio(self, audio):
-    #     CHUNK = 1024
-
-    #     f = io.BytesIO()
-    #     f.write(audio)
-    #     f.seek(0)
-    #     wf = wave.Wave_read(f)
-
-    #     print(wf.getframerate())
-    #     print(wf.getsampwidth())
-
-    #     p = pyaudio.PyAudio()
-
-    #     stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-    #                     channels=wf.getnchannels(),
-    #                     rate=wf.getframerate(),
-    #                     output=True)
-
-    #     data = wf.readframes(CHUNK)
-
-    #     while data != b'':
-    #         stream.write(data)
-    #         data = wf.readframes(CHUNK)
-
-    #     stream.stop_stream()
-    #     stream.close()
-    #     p.terminate()
+        if not Path('.cache-audio').is_dir():
+            os.mkdir('.cache-audio')
 
     def play(self, text):
-        text = text.lower()
-        audio = self.ttsAudio.get(text)
-        if audio == None:
-            print('audio not found')
-            audio = self.translator.get_audio(text)
 
-            # audio = self.translator.speak(
-            #     text, "en-AU", "Catherine", "riff-16khz-16bit-mono-pcm")
-            self.ttsAudio[text] = audio
+        digest = hashlib.md5(text.encode()).hexdigest()
+        audio = self.ttsAudio.get(digest)
+
+        if audio == None:
+            cacheFileName = ".cache-audio/{}.wav".format(digest)
+
+            if Path(cacheFileName).is_file():
+                with open(cacheFileName, 'rb') as audiofile:
+                    audio = audiofile.read()
+            else:
+                translatedText = self.translateText.translate(text)
+
+                if translatedText is None:
+                    return
+
+                audio = self.translator.get_audio(translatedText)
+
+                if audio is not None:
+                    with open('korean-{}'.format(text) + '.wav', 'wb') as audiofile:
+                        audiofile.write(audio)
+                    with open(cacheFileName, 'wb') as audiofile:
+                        audiofile.write(audio)
+
+            self.ttsAudio[digest] = audio
 
         self.sound = mixer.Sound(audio)
         self.sound.play()
         while mixer.get_busy():
             time.sleep(0.25)
-        # self._playAudio(audio)
